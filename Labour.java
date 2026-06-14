938ad4321a5395d89e71306d921487b8b4700734
Sneha Singh <sssingh2075@gmail.com>
2026-06-14 09:29:59 +0530

    adding labour class

---
+++ Labour.java
@@ -0,0 +1,5 @@
+public class Labour {
+    public static void main(String[] args) {
+
+    }
+}
--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 public class Student {
     public static void main(String[] args) {
-        System.out.println("WE are the future of the nation");
+        System.out.println("WE are the future of the India");
     }
 }
